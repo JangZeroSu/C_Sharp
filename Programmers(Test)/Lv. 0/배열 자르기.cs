5748c95d8ebc325a49a2f977f3b9d3b0a481b384
JangZeroSu <110160071+JangZeroSu@users.noreply.github.com>
2022-10-31 22:40:27 +0900

    Create 배열 자르기.cs

---
+++ Programmers(Test)/Lv. 0/배열 자르기.cs
@@ -0,0 +1,11 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/120833#
+
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = numbers.Skip(num1).Take(num2-num1+1).ToArray();        
+        return answer;
+    }
+}
